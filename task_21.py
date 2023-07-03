8cbcda830c6baed535954d391e01149148a8c66a
Anton Kargapolov <igloooooook@gmail.com>
2023-07-03 23:00:46 +0500

    task 19

--- task_21.py
+++ task_21.py
@@ -3,4 +3,8 @@
 # {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}]
 # Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 
- 
+dctr = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {" V ":"S009"}, {" VIII ":"S007"}]
+lst = set()
+for i in dctr:
+    lst.add(*i.values())
+print(lst)
