b1a40c9c58ac78e9ca0dff6a3a14c06565c2573f
Anton Kargapolov <igloooooook@gmail.com>
2023-07-05 20:23:03 +0500

    task 33 done

--- task_33.py
+++ task_33.py
@@ -15,3 +15,22 @@ for i in range(n):
         lst[i] = min_mark
 
 print(lst)
+
+#с функцией enumirate
+# import random
+
+# n = int(input("Введите количество оценок: "))
+# lst = [random.randint(1, 5) for i in range(n)]
+
+# print(*lst)
+
+# max_num = max(lst)
+# min_num = min(lst)
+
+# print(list(enumerate(lst)))
+
+# for i, val in enumerate(lst):
+# if val == max_num:
+# lst[i] = min_num
+
+# print(*lst)
